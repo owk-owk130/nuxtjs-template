3b389f41b999498e8a0266d914b8af9919a56e7f
大脇健吾 <owaki-kengo@kayac.com>
2019-11-06 17:34:48 +0900

    format

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,68 +1,75 @@
 const pkg = require('./package')
 
 module.exports = {
-  mode: 'universal',
+    mode: 'universal',
 
-  /*
+    /*
   ** Headers of the page
   */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-  },
+    head: {
+        title: pkg.name,
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: pkg.description
+            }
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    },
 
-  /*
+    /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+    loading: { color: '#fff' },
 
-  /*
+    /*
   ** Global CSS
   */
-  css: [],
+    css: [],
 
-  /*
+    /*
   ** Plugins to load before mounting the App
   */
-  plugins: [],
+    plugins: [],
 
-  buildModules: ['@nuxt/typescript-build'],
-  /*
+    buildModules: ['@nuxt/typescript-build'],
+    /*
   ** Nuxt.js modules
   */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
-  ],
-  /*
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios'
+    ],
+    /*
   ** Axios module configuration
   */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-  },
+    axios: {
+        // See https://github.com/nuxt-community/axios-module#options
+    },
 
-  /*
+    /*
   ** Build configuration
   */
-  build: {
-    /*
+    build: {
+        /*
     ** You can extend webpack config here
     */
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+        extend(config, ctx) {
+            // Run ESLint on save
+            if (ctx.isDev && ctx.isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+        }
     }
-  }
 }
